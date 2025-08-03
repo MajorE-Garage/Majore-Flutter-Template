f7b7b7f92b10e72f1741de85e5f4282c95afdef6
Meghatronics <emmanuel.1aliyu@gmail.com>
2025-08-03 11:08:15 +0100

    chore(setup): add firebase

--- lib/main/environment_config.dart
+++ lib/main/environment_config.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/services.dart';
+
 class EnvironmentConfig {
   // App Info
-  static const isProd = bool.fromEnvironment('IS_PROD');
+  static const isProd = bool.fromEnvironment('IS_PROD') || appFlavor == 'prod';
   static const appName = String.fromEnvironment('APP_NAME');
+  static const flavor = appFlavor ?? '';
 
   // Connection Info
   static const apiUrl = String.fromEnvironment('API_URL');
--- lib/main/main.dart
+++ lib/main/main.dart
@@ -7,12 +7,19 @@ import '../core/presentation/theming/app_theme_manager.dart';
 import '../core/service_locator/service_locator.dart';
 import '../services/error_logging_service/error_logging_service.dart';
 import 'application.dart';
+import 'environment_config.dart';
+import 'firebase_options_prod.dart' as prod;
+import 'firebase_options_staging.dart' as staging;
 
 void main() {
   runZonedGuarded(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
-      Firebase.initializeApp();
+      Firebase.initializeApp(
+        options: EnvironmentConfig.isProd
+            ? prod.DefaultFirebaseOptions.currentPlatform
+            : staging.DefaultFirebaseOptions.currentPlatform,
+      );
       await ServiceLocator.registerDependencies();
       await AppThemeManager.initialise();
       runApp(const ThisApplication());
